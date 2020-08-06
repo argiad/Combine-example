231140eb5874d15b5289d9e26ce52f2b4e37a16e
argiad <a.s.mkrtchyan@gmail.com>
2020-08-05 17:15:32 -0700

    first attempt

---
+++ Combine example/CombineViewModel.swift
@@ -0,0 +1,15 @@
+//
+//  CombineViewModel.swift
+//  Combine example
+//
+//  Created by Artem Mkrtchyan on 8/5/20.
+//
+
+import Foundation
+import Combine
+
+class CombineViewModel: ObservableObject {
+    
+    @Published var text = ""
+    
+}
--- Combine example/ViewController.swift
+++ Combine example/ViewController.swift
@@ -6,14 +6,41 @@
 //
 
 import UIKit
+import Combine
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var lbl: UILabel!
+    @IBOutlet weak var textfield: UITextField!
+    
+    lazy var viewModel: CombineViewModel = {
+        let model = CombineViewModel()
+        return model
+    }()
+    private var cancellables: Set<AnyCancellable> = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        bindView()
     }
 
+    func bindView() {
+        
+        viewModel.$text.sink{ [weak self] v in
+            self?.lbl.text = v
+        }.store(in: &cancellables)
+        
+        
+        textfield.addTarget(self, action: #selector(ViewController.textFieldDidChange(_:)), for: .allEditingEvents)
+        
+    }
 
+}
+
+extension ViewController {
+    @objc func textFieldDidChange(_ textField: UITextField) {
+        viewModel.text = textfield.text ?? ""
+    }
 }
 
